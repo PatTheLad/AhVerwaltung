efb3b41427eb0084978a22b817af840b202f5fb7
Steve260920 <steve-marvin-r@web.de>
2023-10-16 12:24:58 +0200

    Änderungen Kunden

--- AhVerwaltung/Data/DataKunden.cs
+++ AhVerwaltung/Data/DataKunden.cs
@@ -14,8 +14,84 @@ namespace AhVerwaltung.Data
             kundenListe.Add(new Kunden() { Kdnr = 4, Kurzname = "CMa", Vorname = "Cahrl", Nachname = "Mahgs", Geburtsdatum = new DateTime(1848, 03, 01) });
             kundenListe.Add(new Kunden() { Kdnr = 5, Kurzname = "JWh", Vorname = "Joe", Nachname = "Who", Geburtsdatum = new DateTime(1990, 09, 06) });
             kundenListe.Add(new Kunden() { Kdnr = 6, Kurzname = "JMa", Vorname = "Joe", Nachname = "Mama", Geburtsdatum = new DateTime(2000, 02, 04) });
+            kundenListe.Add(new Kunden() { Kdnr = 7, Kurzname = "DDa", Vorname = "Dreckiger", Nachname = "Dan", Geburtsdatum = new DateTime(1993, 08, 07) });
+            kundenListe.Add(new Kunden() { Kdnr = 8, Kurzname = "SLa", Vorname = "Spitzkopf", Nachname = "Larry", Geburtsdatum = new DateTime(1994, 11, 10) });
+            for(int i = 0; i < 30; i++)
+            {
+                kundenListe.Add(RandomKunde(9 + i));
+            }
             return kundenListe;
 
+        }
+
+        List<String> vornamenListe = new()
+        {
+            "Anna",
+            "Frieda",
+            "Martha",
+            "Erna",
+            "Emma",
+            "Marie",
+            "Gertrud",
+            "Margarete",
+            "Maria",
+            "Elisabeth",
+            "Berta",
+            "Karl",
+            "Wilhelm",
+            "Heinrich",
+            "Hans",
+            "Otto",
+            "Hermann",
+            "Paul",
+            "Walter",
+            "Friedrich",
+            "Ernst",
+            "Willi"
+        };
+
+        List<String> nachnamenListe = new()
+        {
+            "Melchior",
+            "Goldberg",
+            "Stellmacher",
+            "Reder ",
+            "Lipinski",
+            "Eppler",
+            "Beller",
+            "Schrage",
+            "Klingbeil",
+            "Spörl",
+            "Fett",
+            "Eckhoff",
+            "Rentsch",
+            "Husemann",
+            "Breier",
+            "Schimmelpfennig",
+            "Raupach",
+            "Hinzmann",
+            "Henrichs",
+            "Forstner",
+            "Dose"
+        };
+
+        public Kunden RandomKunde(int x)
+        {
+            Random rnd = new Random();
+      
+            String vorname = vornamenListe[rnd.Next(0, vornamenListe.Count())];
+            String nachname = nachnamenListe[rnd.Next(0, nachnamenListe.Count())];
+            String kurzname = vorname.Substring(0, 1) + nachname.Substring(0, 2);
+            Kunden kunde = new Kunden() { Kdnr = x, Kurzname = kurzname, Vorname = vorname, Nachname = nachname, Geburtsdatum = RandomDay() };
+            return kunde;
+        }
+
+        private Random gen = new Random();
+        DateTime RandomDay()
+        {
+            DateTime start = new DateTime(2004, 12, 31);
+          
+            return start.AddDays(-gen.Next(1,20000));
         }
     }
 }
