4cf68fe121c3cfdfcbda67f1331e56cfea9116d2
Jonas Schumacher <ITT421.Jonas.Schumacher@bkt.siwiwissen.de>
2023-10-16 11:49:17 +0200

    Buttons hinzugefügt

--- AhVerwaltung/Data/RückgabeformularData.cs
+++ AhVerwaltung/Data/RückgabeformularData.cs
@@ -4,81 +4,18 @@ namespace AhVerwaltung.Data
 {
     public class RückgabeformularData
     {
-        Rückgabeformular rückgabeformular1 = new Rückgabeformular()
-        {
-            RückgabeFormularNr = 1,
-            Anhänger = new Model.Anhänger() { Kennzeichen = "" },
-            Vertrag = new Vertrag() { VertragsNr = 1 },
-            Vertragskunden = new Kunden() { Kurzname = "BGa" }
-        };
-        Rückgabeformular rückgabeformular2 = new Rückgabeformular()
-        {
-            RückgabeFormularNr = 2,
-            Anhänger = new Anhänger() { Kennzeichen = "" },
-            Vertrag = new Vertrag() { VertragsNr = 1 },
-            Vertragskunden = new Kunden() { Kdnr = 1 }
-        };
-        Rückgabeformular rückgabeformular3 = new Rückgabeformular()
-        {
-            RückgabeFormularNr = 3,
-            Anhänger = new Anhänger() { Kennzeichen = "" },
-            Vertrag = new Vertrag() { VertragsNr = 1 },
-            Vertragskunden = new Kunden() { Kdnr = 1 }
-        };
-        Rückgabeformular rückgabeformular4 = new Rückgabeformular()
-        {
-            RückgabeFormularNr = 4,
-            Anhänger = new Anhänger() { Kennzeichen = "" },
-            Vertrag = new Vertrag() { VertragsNr = 1 },
-            Vertragskunden = new Kunden() { Kdnr = 1 }
-        };
-        Rückgabeformular rückgabeformular5 = new Rückgabeformular()
-        {
-            RückgabeFormularNr = 5,
-            Anhänger = new Anhänger() { Kennzeichen = "" },
-            Vertrag = new Vertrag() { VertragsNr = 1 },
-            Vertragskunden = new Kunden() { Kdnr = 1 }
-        };
 
-        public static List<Rückgabeformular> GetRückgabeformular()
+        public static List<Rückgabeformular> GetRueckgabeformular(int anzahl)
         {
-            List<Rückgabeformular> rückgabeListe = new();
-            rückgabeListe.Add(new Rückgabeformular()
+            Random rndm = new();
+            List<Rückgabeformular> tmpList = new();
+            int count = 0;
+            while (anzahl > count)
             {
-                RückgabeFormularNr = 1,
-                Anhänger = new Anhänger() { Kennzeichen = "" },
-                Vertrag = new Vertrag() { VertragsNr = 1 },
-                Vertragskunden = new Kunden() { Kurzname = "BGa" }
-            });
-            rückgabeListe.Add(new Rückgabeformular()
-            {
-                RückgabeFormularNr = 2,
-                Anhänger = new Anhänger() { Kennzeichen = "" },
-                Vertrag = new Vertrag() { VertragsNr = 1 },
-                Vertragskunden = new Kunden() { Kurzname = "MZu" }
-            });
-            rückgabeListe.Add(new Rückgabeformular()
-            {
-                RückgabeFormularNr = 3,
-                Anhänger = new Anhänger() { Kennzeichen = "" },
-                Vertrag = new Vertrag() { VertragsNr = 1 },
-                Vertragskunden = new Kunden() { Kurzname = "BOb" }
-            });
-            rückgabeListe.Add(new Rückgabeformular()
-            {
-                RückgabeFormularNr = 4,
-                Anhänger = new Anhänger() { Kennzeichen = "" },
-                Vertrag = new Vertrag() { VertragsNr = 1 },
-                Vertragskunden = new Kunden() { Kurzname = "CMa" }
-            });
-            rückgabeListe.Add(new Rückgabeformular()
-            {
-                RückgabeFormularNr = 5,
-                Anhänger = new Anhänger() { Kennzeichen = "" },
-                Vertrag = new Vertrag() { VertragsNr = 1 },
-                Vertragskunden = new Kunden() { Kurzname = "JWh" }
-            });
-            return rückgabeListe;
+                tmpList.Add(new Rückgabeformular() { RückgabeFormularNr = rndm.Next(1, 999), Anhänger = new Anhänger() { Kennzeichen = $"OE-AB-{rndm.Next(1, 999)}" }, Vertrag = new Vertrag() { VertragsNr = rndm.Next(1, 999) }, Vertragskunden = new Kunden() { Kdnr = rndm.Next(1, 999) } });
+                count++;
+            }
+            return tmpList;
         }
     }
 }
